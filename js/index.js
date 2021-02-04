2969d733cf6c5696348e307fabac877e48eccccb
xy <1425662757@qq.com>
2021-02-04 14:48:38 +0800

    1

--- js/index.js
+++ js/index.js
@@ -74,7 +74,7 @@ var top250 = {
         $node.find('.difector').text(value.difector)
         _this.$container.append($node);
 
-    })
+        })
     },
     isToBottom : function(){
         return this.$section.scrollTop() + this.$section.height()  >= this.$container.height() -10 ;
@@ -168,35 +168,34 @@ var search = {
         this.$button.on('click',function(){
              _this.key = _this.$text.val();
              _this.start();
-             _this.render();
+            _this.$container.empty()
         })
     },
     render : function(data){
         var _this = this;
         data.forEach(function(value,index){
-            var tal = `<div class="item">
+        var tal = `<div class="item">
             <a href="#">
                 <div class="cover">
                     <img src="https://images.weserv.nl/?url=img2.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg" alt="">
                 </div>
                 <div class="datail">
                     <h2 class="name">肖申克的救赎</h2>
-                    <div class="extra">上映时间：<span class="date">02月09日</span></div>
-                    <div class="extra"><span class = "see">3690人想看</see></div>
-                    <div class="extra">类型：<span class = "plot">剧情</span></div>
-                    <div class="extra">国家：<span class = "region">匈牙利</span></div>
+                    <div class="extra"><span class="score">9.7</span>分</div>
                 </div>
             </a>
         </div>
         `
-            var $node = $(tal);
-            $node.find('.cover img').attr('src',_this.getImgUrl(value.img))
-            $node.find('h2').text(value.title)
-            $node.find('.date').text(value.date)
-            $node.find('.plot').text(value.plot)
-            $node.find('.region').text(value.region)
-            _this.$container.append($node);
+        var $node = $(tal);
+        $node.find('.cover img').attr('src',_this.getImgUrl(value.img))
+        $node.find('h2').text(value.title)
+        $node.find('.score').text(value.rating)
+        $node.find('.quote').text(value.quote)
+        $node.find('.difector').text(value.difector)
+        _this.$container.append($node);
+
         })
+       
     },
     start : function(){
         var _this = this
@@ -215,11 +214,17 @@ var search = {
             dataType : 'json'
         }).done(function(ret){
             console.log(ret)
-            callback(ret.valie.subject);
+            callback(ret.data.subject);
         }).fail(function(){
             _this.$container.text('获取数据失败');
         })
     },
+    getImgUrl : function(url){
+        if(url){
+        let _u = url.substring(7);
+        return 'https://images.weserv.nl/?url=' + _u;
+        }
+    }
 
 }
 
@@ -238,7 +243,7 @@ var app = {
         var _this = this
         this.$tabs.on('click',function(){
             $(this).addClass('active').siblings().removeClass('active')
-            _this.$panels.hide().eq($(this).index()).fadeIn(1000);
+            _this.$panels.hide().eq($(this).index()).fadeIn(100);
         })
     },
     start : function(){
